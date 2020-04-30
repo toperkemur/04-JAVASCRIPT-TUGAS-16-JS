51a32b12bbaa3d79972cafc2dd63a1b909a3fe76
Christopher Andrew Kemur <christopher.kemur@gmail.com>
2020-04-30 17:14:39 +0700

    Initial Commit
    Menambahkan tugas16.js

---
+++ tugas16.js
@@ -0,0 +1,12 @@
+var deret1 = [45, 100, 99, 3, 2, 4, 1, 2, 3, 19, 22, 14, 10 ];
+var deret2 = [45, 100, 99, 3, 2, 81, 22, 55, 1, 2, 3, 4, 5 ];
+
+console.log("Deret 1");
+console.log(45, 100, 99, 3, 2, 4, 1, 2, 3, 19, 22, 14, 10 )
+console.log("Nilai Minimal : ",Math.min(...deret1));
+console.log("Nilai Maximal : ",Math.max(...deret1));
+console.log("========================================")
+console.log("Deret 2");
+console.log(45, 100, 99, 3, 2, 81, 22, 55, 1, 2, 3, 4, 5 )
+console.log("Nilai Minimal : ",Math.min(...deret2));
+console.log("Nilai Maximal : ",Math.max(...deret2));
